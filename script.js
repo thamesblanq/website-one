a8d87862c96581d025f70a7a74f7c4afa6c7848c
blanq31 <obiwulugodswill@gmail.com>
2023-02-24 07:22:47 +0100

    UI modifications

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const toggleBtn = document.querySelector("#toggle-btn");
-const li =document.querySelector('.li-list')
+const li = document.querySelector('.li-list')
 
 
 
 toggleBtn.addEventListener("click", () => {
     li.classList.toggle('show');
-    if (li.classList.contains('show')) {
+  /*   if (li.classList.contains('show')) {
         toggleBtn.classList.remove('fa fa-bars');
         toggleBtn.classList.add('fa-solid fa-xmark');
     }else {
         toggleBtn.classList.remove('fa-solid fa-xmark');
         toggleBtn.classList.add('fa fa-bars');   
-    }
+    } */
 })
 
